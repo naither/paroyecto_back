e64a75f4d0d12f8786358fde2782f5ea868e6d9d
jimmy <naither@gmail.com>
2024-08-09 19:54:42 -0400

    actualiza security handler

--- api/middlewares/security.handler.js
+++ api/middlewares/security.handler.js
@@ -5,3 +5,13 @@ function securityHandler(app) {
 }
 
 module.exports = securityHandler;
+/*//¿Para qué sirve este código?
+El propósito de este código es fortalecer la seguridad
+ de una aplicación web de Node.js 
+ al usar helmet. helmet configura 
+ varios encabezados HTTP para proteger 
+ la aplicación de vulnerabilidades comunes, 
+ como ataques XSS, clickjacking, y sniffing de MIME type, 
+ entre otros. Este código encapsula la configuración 
+ de helmet en una función que puede ser 
+ fácilmente aplicada a cualquier aplicación Express.*/
